0778265212415bd0c1a14d62283d1616e4cae5c3
vrees <viktor.rees@gmx.de>
2019-08-02 19:05:13 +0200

    BMP280 Senor and CayenneLPP added

--- ttgo-mcci-otaa/src/main.cpp
+++ ttgo-mcci-otaa/src/main.cpp
@@ -32,10 +32,13 @@
  *******************************************************************************/
 
 #include <Arduino.h>
+#include <CayenneLPP.h>
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
 #include <U8x8lib.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from ttnctl output, this means to reverse
@@ -63,6 +66,12 @@ static osjob_t sendjob;
 const unsigned TX_INTERVAL = 30;
 //For TTGO LoRa32 V2 use:
 U8X8_SSD1306_128X64_NONAME_HW_I2C display(/*rst*/ U8X8_PIN_NONE);
+CayenneLPP lpp(51);
+
+// Sensors
+#define BMP_SDA 21
+#define BMP_SCL 22
+Adafruit_BME280 bmp; // I2C
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
@@ -72,58 +81,63 @@ const lmic_pinmap lmic_pins = {
     //.rst = LMIC_UNUSED_PIN,
     .dio = {/*dio0*/ 26, /*dio1*/ 33, /*dio2*/ 32}};
 
-
+void readSensorValues();
 void do_send(osjob_t *j);
 void loop();
+void setupSensors();
 
-void onEvent (ev_t ev) {
+void onEvent(ev_t ev)
+{
     Serial.print(os_getTime());
     Serial.print(": ");
-    switch(ev) {
-        case EV_SCAN_TIMEOUT:
-            Serial.println(F("EV_SCAN_TIMEOUT"));
-            break;
-        case EV_BEACON_FOUND:
-            Serial.println(F("EV_BEACON_FOUND"));
-            break;
-        case EV_BEACON_MISSED:
-            Serial.println(F("EV_BEACON_MISSED"));
-            break;
-        case EV_BEACON_TRACKED:
-            Serial.println(F("EV_BEACON_TRACKED"));
-            break;
-        case EV_JOINING:
-            Serial.println(F("EV_JOINING"));
-            break;
-        case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
+    switch (ev)
+    {
+    case EV_SCAN_TIMEOUT:
+        Serial.println(F("EV_SCAN_TIMEOUT"));
+        break;
+    case EV_BEACON_FOUND:
+        Serial.println(F("EV_BEACON_FOUND"));
+        break;
+    case EV_BEACON_MISSED:
+        Serial.println(F("EV_BEACON_MISSED"));
+        break;
+    case EV_BEACON_TRACKED:
+        Serial.println(F("EV_BEACON_TRACKED"));
+        break;
+    case EV_JOINING:
+        Serial.println(F("EV_JOINING"));
+        break;
+    case EV_JOINED:
+        Serial.println(F("EV_JOINED"));
+        {
+            u4_t netid = 0;
+            devaddr_t devaddr = 0;
+            u1_t nwkKey[16];
+            u1_t artKey[16];
+            LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+            Serial.print("netid: ");
+            Serial.println(netid, DEC);
+            Serial.print("devaddr: ");
+            Serial.println(devaddr, HEX);
+            Serial.print("artKey: ");
+            for (int i = 0; i < sizeof(artKey); ++i)
             {
-              u4_t netid = 0;
-              devaddr_t devaddr = 0;
-              u1_t nwkKey[16];
-              u1_t artKey[16];
-              LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
-              Serial.print("netid: ");
-              Serial.println(netid, DEC);
-              Serial.print("devaddr: ");
-              Serial.println(devaddr, HEX);
-              Serial.print("artKey: ");
-              for (int i=0; i<sizeof(artKey); ++i) {
                 Serial.print(artKey[i], HEX);
-              }
-              Serial.println("");
-              Serial.print("nwkKey: ");
-              for (int i=0; i<sizeof(nwkKey); ++i) {
-                Serial.print(nwkKey[i], HEX);
-              }
-              Serial.println("");
             }
-            // Disable link check validation (automatically enabled
-            // during join, but because slow data rates change max TX
-	    // size, we don't use it in this example.
-            LMIC_setLinkCheckMode(0);
-            break;
-        /*
+            Serial.println("");
+            Serial.print("nwkKey: ");
+            for (int i = 0; i < sizeof(nwkKey); ++i)
+            {
+                Serial.print(nwkKey[i], HEX);
+            }
+            Serial.println("");
+        }
+        // Disable link check validation (automatically enabled
+        // during join, but because slow data rates change max TX
+        // size, we don't use it in this example.
+        LMIC_setLinkCheckMode(0);
+        break;
+    /*
         || This event is defined but not used in the code. No
         || point in wasting codespace on it.
         ||
@@ -131,41 +145,42 @@ void onEvent (ev_t ev) {
         ||     Serial.println(F("EV_RFU1"));
         ||     break;
         */
-        case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
-            break;
-        case EV_REJOIN_FAILED:
-            Serial.println(F("EV_REJOIN_FAILED"));
-            break;
-        case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            if (LMIC.txrxFlags & TXRX_ACK)
-              Serial.println(F("Received ack"));
-            if (LMIC.dataLen) {
-              Serial.print(F("Received "));
-              Serial.print(LMIC.dataLen);
-              Serial.println(F(" bytes of payload"));
-            }
-            // Schedule next transmission
-            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-            break;
-        case EV_LOST_TSYNC:
-            Serial.println(F("EV_LOST_TSYNC"));
-            break;
-        case EV_RESET:
-            Serial.println(F("EV_RESET"));
-            break;
-        case EV_RXCOMPLETE:
-            // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
-            break;
-        case EV_LINK_DEAD:
-            Serial.println(F("EV_LINK_DEAD"));
-            break;
-        case EV_LINK_ALIVE:
-            Serial.println(F("EV_LINK_ALIVE"));
-            break;
-        /*
+    case EV_JOIN_FAILED:
+        Serial.println(F("EV_JOIN_FAILED"));
+        break;
+    case EV_REJOIN_FAILED:
+        Serial.println(F("EV_REJOIN_FAILED"));
+        break;
+    case EV_TXCOMPLETE:
+        Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+        if (LMIC.txrxFlags & TXRX_ACK)
+            Serial.println(F("Received ack"));
+        if (LMIC.dataLen)
+        {
+            Serial.print(F("Received "));
+            Serial.print(LMIC.dataLen);
+            Serial.println(F(" bytes of payload"));
+        }
+        // Schedule next transmission
+        os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+        break;
+    case EV_LOST_TSYNC:
+        Serial.println(F("EV_LOST_TSYNC"));
+        break;
+    case EV_RESET:
+        Serial.println(F("EV_RESET"));
+        break;
+    case EV_RXCOMPLETE:
+        // data received in ping slot
+        Serial.println(F("EV_RXCOMPLETE"));
+        break;
+    case EV_LINK_DEAD:
+        Serial.println(F("EV_LINK_DEAD"));
+        break;
+    case EV_LINK_ALIVE:
+        Serial.println(F("EV_LINK_ALIVE"));
+        break;
+    /*
         || This event is defined but not used in the code. No
         || point in wasting codespace on it.
         ||
@@ -173,35 +188,38 @@ void onEvent (ev_t ev) {
         ||    Serial.println(F("EV_SCAN_FOUND"));
         ||    break;
         */
-        case EV_TXSTART:
-            Serial.println(F("EV_TXSTART"));
-            break;
-        default:
-            Serial.print(F("Unknown event: "));
-            Serial.println((unsigned) ev);
-            break;
+    case EV_TXSTART:
+        Serial.println(F("EV_TXSTART"));
+        break;
+    default:
+        Serial.print(F("Unknown event: "));
+        Serial.println((unsigned)ev);
+        break;
     }
 }
 
-void do_send(osjob_t* j){
+void do_send(osjob_t *j)
+{
     // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & OP_TXRXPEND) {
+    if (LMIC.opmode & OP_TXRXPEND)
+    {
         Serial.println(F("OP_TXRXPEND, not sending"));
-    } else {
-        // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 1);
+    }
+    else
+    {
+        readSensorValues();
+        LMIC_setTxData2(1, lpp.getBuffer(), lpp.getSize(), 1);
         Serial.println(F("Packet queued"));
     }
     // Next TX is scheduled after TX_COMPLETE event.
 }
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     Serial.println(F("Starting ..."));
 
-    display.begin();
-    display.setFont(u8x8_font_chroma48medium8_r);
-    display.drawString(0, 3, "go go");
+    setupSensors();
 
     // LMIC init
     os_init();
@@ -212,6 +230,44 @@ void setup() {
     do_send(&sendjob);
 }
 
-void loop() {
+void setupSensors()
+{
+    display.begin();
+    display.setFont(u8x8_font_chroma48medium8_r);
+    display.drawString(0, 3, "Starting");
+
+    if (!bmp.begin())
+    {
+        Serial.println("Could not find a valid BMP280 !");
+        while (1)
+            ;
+    }
+}
+void loop()
+{
     os_runloop_once();
+}
+
+void readSensorValues()
+{
+    float tempertur = bmp.readTemperature();
+    float pressure = bmp.readPressure() / 100;
+    float altitude = bmp.readAltitude(1013.25);
+
+    Serial.print("T = ");
+    Serial.print(tempertur);
+    Serial.println(" °C");
+
+    Serial.print("P = ");
+    Serial.print(pressure);
+    Serial.println(" hPa ");
+
+    Serial.print("H = ");
+    Serial.print(altitude);
+    Serial.println(" m");
+
+    lpp.reset();
+    lpp.addTemperature(1, tempertur);
+    lpp.addRelativeHumidity(2, pressure);
+    lpp.addAnalogOutput(3, altitude);
 }
